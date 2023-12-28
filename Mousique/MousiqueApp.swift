d6573907fbef7998a99b46bbf0f6d69dfea29d14
CoderLLImoder <kaperskiyi@gmail.com>
2023-12-28 14:37:03 +0300

    Initial Commit

---
+++ Mousique/MousiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  MousiqueApp.swift
+//  Mousique
+//
+//  Created by Илья Капёрский on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MousiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
